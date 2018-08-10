94b2d842706a5d8acd9837f8291e81184dfcbff1
ryu1 <kokubo.ryuichi@ntt-it.co.jp>
2018-08-10 11:17:53 +0900

    build: add flycheck-clang config.

---
+++ test/.dir-locals.el
@@ -0,0 +1,10 @@
+
+((nil . ((flycheck-clang-language-standard . "c++11")
+         (flycheck-clang-include-path . ("."
+					 "googletest/googletest/include"
+					 "googletest/googlemock/include"
+                                         "../src"
+                                         "../include"
+                                         ))
+         )
+      ))
