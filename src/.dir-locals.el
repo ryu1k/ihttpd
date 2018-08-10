39dfc0be1f64e86203dbd3d4c2433d066405dc4d
ryu1 <kokubo.ryuichi@ntt-it.co.jp>
2018-08-10 11:56:51 +0900

    build: add src dir and make facility.

---
+++ src/.dir-locals.el
@@ -0,0 +1,7 @@
+
+((nil . ((flycheck-clang-language-standard . "c++11")
+         (flycheck-clang-include-path . ("."
+                                         "../include"
+                                         ))
+         )
+      ))
